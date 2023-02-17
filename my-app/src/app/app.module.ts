71d61b93a824293ebd5ecb985e63adea90b82b3b
PacoBarni <paco12cv@hotmail.com>
2023-02-17 10:11:48 +0100

    test

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Component1Component } from './components/component1/component1.component';
+import { Directive1Directive } from './directives/directive1.directive';
+import { Pipe1Pipe } from './pipes/pipe1.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Component1Component,
+    Directive1Directive,
+    Pipe1Pipe
   ],
   imports: [
     BrowserModule,
